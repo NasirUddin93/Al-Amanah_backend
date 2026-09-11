2532fddd861b6b51229613ed461e924b11105e89
NasirUddin93 <nasir93cse@gmail.com>
2026-09-11 13:50:50 +0600

    Some local change

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,7 +37,12 @@ return [
             'throw' => false,
             'report' => false,
         ],
-
+        // 'public' => [
+        //     'driver' => 'local',
+        //     'root' => public_path('uploads'),
+        //     'url' => env('APP_URL').'/uploads',
+        //     'visibility' => 'public',
+        // ],
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
